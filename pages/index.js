dd951de1dde88128f722dc704793c07baa6094c8
Bruno Azevedo <49662128+bras41@users.noreply.github.com>
2026-04-10 21:37:46 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Correção da mensagem de teste! - Atualizado em 09/04/2026 😎</h1>
+function Home() {
+  return <h1>Correção da mensagem de teste! - Atualizado em 09/04/2026 😎</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
